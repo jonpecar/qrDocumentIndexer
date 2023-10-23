e07f0958a45c7db3d477957f87ddfd76fd556672
Jonathan Pecar <jonathan.pecar@outlook.com>
2023-10-23 13:33:30 +1100

    Implemented QR based sorting through GUI

--- qrDocumentIndexer/gui/launch_window.py
+++ qrDocumentIndexer/gui/launch_window.py
@@ -1,7 +1,12 @@
 import customtkinter as ctk
+
 from .file_select.file_select_window import FileSelectWindow
+
 from .stamp_options_frame import StampOptionsFrame
 from .stamp_action_handler import StampActionHandler
+
+from .sort_actions_handler import SortActionHandler
+from .sort_options_frame import SortOptionsFrame
 
 ADD_PDF_TITLE = 'Add QR Codes to PDFs'
 SORT_SCANNED_DOCS_TITLE = 'Sort Scanned Documents'
@@ -23,4 +28,6 @@ class LaunchWindow(ctk.CTkFrame):
         FileSelectWindow(self, ADD_PDF_TITLE, ADD_PDF_TITLE, StampOptionsFrame, StampActionHandler)
 
     def click_sort_button(self):
-        pass
+        FileSelectWindow(self, SORT_SCANNED_DOCS_TITLE, SORT_SCANNED_DOCS_TITLE, SortOptionsFrame, SortActionHandler,
+                         '.pdf', 
+                         [['PDF Files','*.pdf'], ['Image Files', ['*.jpg','*.jpeg','*.png','*.tiff','*.tif']],['All Files', '*.*']])
